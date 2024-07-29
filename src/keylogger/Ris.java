ffd5c5c47386f23e355d388f5ee4bf2596785c08
Andrei Romanov <you@example.com>
2024-07-29 09:33:07 +0300

    КейЛоггер2

--- src/keylogger/Ris.java
+++ src/keylogger/Ris.java
@@ -20,26 +20,18 @@ public class Ris extends JPanel implements KeyListener {
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-    }
-
-    @Override
     public void keyTyped(KeyEvent e) {
+        File file = new File("names.txt");
+        try (OutputStreamWriter outputStream = new OutputStreamWriter(new FileOutputStream(file, true))) {
+            outputStream.write(e.getKeyChar());
+        } catch (Exception u) {
+            u.printStackTrace();
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        File file=new File("names.txt");
-        char a=e.getKeyChar();
-        if ((a+"").matches("[a-zA-Z]| |\\d*")){
-        try (OutputStream outputStream=new FileOutputStream(file,true)){
-            outputStream.write(e.getKeyChar());
-        }catch (Exception u){
-            u.printStackTrace();
-        }
-        System.out.println(e.getKeyCode());}
+
     }
 
     @Override
