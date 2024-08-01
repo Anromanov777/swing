284123544ba0d735056dfd094820230462b01458
Andrei Romanov <you@example.com>
2024-08-01 23:48:53 +0300

    Выбор разрешения

---
+++ src/viborrazreshenia/Main.java
@@ -0,0 +1,18 @@
+package viborrazreshenia;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] s = {"800x600", "1024x768", "1200x600", "1280x1024", "1680x1050", "1920x1080"};
+        String[] mas = new String[2];
+
+        Object res = JOptionPane.showInputDialog(null, "Выберите разрешение", "Выбор разрешения", JOptionPane.QUESTION_MESSAGE, null, s, s[0]);
+        mas = ((String) res).split("x");
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(Integer.parseInt(mas[0]), Integer.parseInt(mas[1]));
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
