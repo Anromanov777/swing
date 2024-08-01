ef5ebe1f5fd9fc85a51b54c7581054e34bc59be4
Andrei Romanov <you@example.com>
2024-08-01 23:50:00 +0300

    Выбор разрешения

--- src/viborrazreshenia/Main.java
+++ src/viborrazreshenia/Main.java
@@ -5,8 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         String[] s = {"800x600", "1024x768", "1200x600", "1280x1024", "1680x1050", "1920x1080"};
-        String[] mas = new String[2];
-
+        String[] mas;
         Object res = JOptionPane.showInputDialog(null, "Выберите разрешение", "Выбор разрешения", JOptionPane.QUESTION_MESSAGE, null, s, s[0]);
         mas = ((String) res).split("x");
         JFrame frame = new JFrame();
